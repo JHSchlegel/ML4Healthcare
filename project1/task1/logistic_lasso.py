c12f4dabb106989330598dc2b842f4ba43ffd19d
Jan Schlegel <jschlegel@student.ethz.ch>
2024-03-19 21:24:41 +0100

    created file for logistic lasso

---
+++ project1/task1/logistic_lasso.py
@@ -0,0 +1,12 @@
+# This file contains the code for subtask 2 of task 1 of project 1 in
+# Machine Learning for Healthcare offered at ETH Zurich in FS2024
+
+## Packages and presets:
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+# %%
+## Load the data:
+
+# %%
