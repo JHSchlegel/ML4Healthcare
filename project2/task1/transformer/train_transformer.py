84abbf3acad133044e45c36eeb7585380b746c9f
Jan Schlegel <jschlegel@student.ethz.ch>
2024-05-10 23:17:02 +0200

    added bash script for training; modified training to not return weights

--- project2/task1/transformer/train_transformer.py
+++ project2/task1/transformer/train_transformer.py
@@ -67,11 +67,10 @@ def main():
     #!!! copied from https://www.kaggle.com/code/megazotya/ecg-transformer/notebook
     # We observed that oversampling the minority class tended to improve
     # the model's performance by a tiny bit.
-    if config["preprocessing"]["use_smote"]:
-        sm = SMOTE(random_state=config["general"]["seed"])
-        X_train, y_train = sm.fit_resample(X_train, y_train)
-        ic(X_train.shape, y_train.shape)
-        ic(np.unique(y_train, return_counts=True))
+    sm = SMOTE(random_state=config["general"]["seed"])
+    X_train, y_train = sm.fit_resample(X_train, y_train)
+    ic(X_train.shape, y_train.shape)
+    ic(np.unique(y_train, return_counts=True))
 
     train_loader = DataLoader(
         PTB_Dataset(X_train, y_train),
